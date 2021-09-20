d4075081af9eff96666a86d109b9663fac04369f
Dan Beck <63600571+db1721@users.noreply.github.com>
2021-09-19 20:27:30 -0400

    20211909 (1:00)

--- expense_tracker_app/src/context/contextReducer.js
+++ expense_tracker_app/src/context/contextReducer.js
@@ -1,5 +1,17 @@
+const transactions = [
+    {id: 1, }
+]
+
 const contextReducer = (state, action) => {
-    
+    switch(action.type)
+    {
+        case 'DELETE_TRANSACTION':
+            break;
+        case 'ADD_TRANSACTION':
+            break;
+        default:
+            break;
+    }
 }
 
 export default contextReducer;
